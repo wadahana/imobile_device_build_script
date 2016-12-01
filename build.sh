93d3a27de9b4f2b52e129bca335170f2e5367aff
wadahana <wadahana@gmail.com>
2016-12-01 13:38:49 +0800

    ideviceinstall patch

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set +x
+
 path=$(cd "$(dirname "$0")"; pwd)
 build_path="${path}/build"
 release_path="${path}/target"
@@ -17,13 +17,33 @@ function build_module()
         git clone "$module_git_url" "$module_path"  > /dev/null
     fi
     pushd "${module_path}" > /dev/null
-        git checkout
+        git checkout 
         git reset --hard HEAD > /dev/null
         ./autogen.sh --prefix="${release_path}" > /dev/null
         make
         make install
     popd > /dev/null
 
+}
+
+function build_ideviceinstaller() 
+{
+    module_name="ideviceinstaller"
+    module_path="${build_path}/${module_name}"
+    module_git_url="https://github.com/libimobiledevice/ideviceinstaller.git"
+    echo -e "chekcout ${module_name} ..."
+    if [ ! -d "$module_path" ]; then
+        git clone "$module_git_url" "$module_path"  > /dev/null
+    fi
+    pushd "${module_path}" > /dev/null
+        git reset --hard
+        git clean -df
+        git checkout 442f670a01f82c63d1316d60f1f47191b8bc6b5a
+        ./autogen.sh --prefix="${release_path}" --enable-static > /dev/null
+        patch -p1 < "${path}/ideviceinstall.patch"
+        make
+        make install
+    popd > /dev/null
 }
 
 function build_openssl()
@@ -36,31 +56,28 @@ function build_openssl()
     fi
     pushd "${openssl_path}" > /dev/null
     git checkout OpenSSL_1_0_2j
-    git reset --hard HEAD > /dev/null
+    git reset --hard > /dev/null
     ./Configure --prefix="${release_path}" darwin64-x86_64-cc  zlib-dynamic shared enable-cms  enable-ec_nistp_64_gcc_128
     make
     make install
 }
 
-#brew install autoconf
-#brew install automake
-#brew install pkg-config
-#brew install libtool
-#brew install libxml2
-#brew install libtasn1
-#brew install libzip
-#brew install libusb
+brew install autoconf
+brew install automake
+brew install pkg-config
+brew install libtool
+brew install libxml2
+brew install libtasn1
+brew install libzip
+brew install libusb
 
 export PKG_CONFIG_PATH="${release_path}/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PATH="${release_path}/bin:$PATH"
 export LD_LIBRARY_PATH="${release_path}/lib:$LD_LIBRARY_PATH"
 export CPATH="${release_path}/include:$LD_LIBRARY_PATH"
-export
-#build_openssl
-#build_module "libplist" "https://github.com/libimobiledevice/libplist.git"
-#build_module "libusbmuxd" "https://github.com/libimobiledevice/libusbmuxd.git"
-#build_module "libimobiledevice" "https://github.com/libimobiledevice/libimobiledevice.git"
+
+build_openssl
+build_module "libplist" "https://github.com/libimobiledevice/libplist.git"
+build_module "libusbmuxd" "https://github.com/libimobiledevice/libusbmuxd.git"
+build_module "libimobiledevice" "https://github.com/libimobiledevice/libimobiledevice.git"
 build_module "usbmuxd" "http://git.sukimashita.com/usbmuxd.git"
-# path to 442f670a01f82c63d1316d60f1f47191b8bc6b5a
-build_module "ideviceinstaller" "https://github.com/libimobiledevice/ideviceinstaller.git"
-set +x
