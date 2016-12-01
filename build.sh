2fd6b63a79055e45e09dce91f40b44eb78bb52e5
wadahana <wadahana@gmail.com>
2016-12-01 15:23:22 +0800

    add usbmuxd.patch

--- build.sh
+++ build.sh
@@ -12,38 +12,25 @@ function build_module()
     module_name=$1
     module_path="${build_path}/${module_name}"
     module_git_url=$2
-    echo -e "chekcout ${module_name} ..."
-    if [ ! -d "$module_path" ]; then
-        git clone "$module_git_url" "$module_path"  > /dev/null
-    fi
-    pushd "${module_path}" > /dev/null
-        git checkout 
-        git reset --hard HEAD > /dev/null
-        ./autogen.sh --prefix="${release_path}" > /dev/null
-        make
-        make install
-    popd > /dev/null
-
-}
-
-function build_ideviceinstaller() 
-{
-    module_name="ideviceinstaller"
-    module_path="${build_path}/${module_name}"
-    module_git_url="https://github.com/libimobiledevice/ideviceinstaller.git"
-    echo -e "chekcout ${module_name} ..."
+    commit_name=$3
+    patch_path=$4
+    echo -e "\n\n\t\tchekcout ${module_name} ...\n\n"
     if [ ! -d "$module_path" ]; then
         git clone "$module_git_url" "$module_path"  > /dev/null
     fi
     pushd "${module_path}" > /dev/null
         git reset --hard
         git clean -df
-        git checkout 442f670a01f82c63d1316d60f1f47191b8bc6b5a
-        ./autogen.sh --prefix="${release_path}" --enable-static > /dev/null
-        patch -p1 < "${path}/ideviceinstall.patch"
+        git checkout "${commit_name}"
+        ./autogen.sh --prefix="${release_path}" > /dev/null
+        echo -e "patch_path: ${patch_path}"
+        if [ -f "${patch_path}" ]; then
+            patch -p1 < "${patch_path}"
+        fi
         make
         make install
     popd > /dev/null
+
 }
 
 function build_openssl()
@@ -80,4 +67,5 @@ build_openssl
 build_module "libplist" "https://github.com/libimobiledevice/libplist.git"
 build_module "libusbmuxd" "https://github.com/libimobiledevice/libusbmuxd.git"
 build_module "libimobiledevice" "https://github.com/libimobiledevice/libimobiledevice.git"
-build_module "usbmuxd" "http://git.sukimashita.com/usbmuxd.git"
+build_module "usbmuxd" "http://git.sukimashita.com/usbmuxd.git" "35e5d48f29ae03b2b9686109e4ed9ab8b9677ce8" "${path}/usbmuxd.patch"
+build_module "ideviceinstaller" "https://github.com/libimobiledevice/ideviceinstaller.git" "442f670a01f82c63d1316d60f1f47191b8bc6b5a" "${path}/ideviceinstaller.patch"
